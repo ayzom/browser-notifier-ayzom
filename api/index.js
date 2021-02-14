185cbe4ed6bd0afa9494c21f5b5ec5e5e1127877
ayzom <59090251+ayzom@users.noreply.github.com>
2021-02-14 15:32:44 +0000

    push subscription changes

--- api/index.js
+++ api/index.js
@@ -1,7 +1,26 @@
 var axios = require('axios');
-const express = require('express');
+const webPush = require("web-push");
+
+const express = require("express");
+var bodyParser = require('body-parser')
+var cors = require('cors')
 
 const app = express();
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+app.use(cors())
+
+const VAPID_PUBLIC_KEY="BBupVJwAFiLot1JDseHWTugXwXqFwycSUZ9s3lEQWhtyG0HGN10e0qd5cn7Ob6FJGFu3pDhivqFn7HGgyDMF8_g";
+const VAPID_PRIVATE_KEY="W3T4oLIY1ahRRjy8serG6RVDreEWLJdXPKEZ9ubv9MU";
+
+webPush.setVapidDetails(
+    'mailto:akhilesh@ayzom.com',
+    VAPID_PUBLIC_KEY,
+    VAPID_PRIVATE_KEY
+  );
 
 app.get('/', (req, res) => res.send('Home Page Route'));
 
@@ -11,27 +30,45 @@ app.get('/portfolio', (req, res) => res.send('Portfolio Page Route'));
 
 app.get('/contact', (req, res) => res.send('Contact Page Route'));
 
-app.get('/save-vapid', async (req, res) => {
+app.post('/notify', async (req, res) => {
+        const subscription = req.body.subscription;
+        const payload = null;
+        const options = {
+            TTL: req.body.ttl,
+        };
+        const config = {
+            method: 'put',
+            url: 'https://ayzompush.firebaseio.com/index.json',
+            headers: { 
+              'Content-Type': 'application/json'
+            },
+            data : JSON.stringify(subscription)
+          };
+          
+          await axios(config)
+          .then(function (response) {
+            console.log(JSON.stringify(response.data));
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
 
-    var data = JSON.stringify({"device":"ihpone"});
-    
-    var config = {
-      method: 'put',
-      url: 'https://ayzompush.firebaseio.com/index.json',
-      headers: { 
-        'Content-Type': 'application/json'
-      },
-      data : data
-    };
-    
-    await axios(config)
-    .then(function (response) {
-      console.log(JSON.stringify(response.data));
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-    
+
+        
+        setTimeout(function () {
+            webPush
+            .sendNotification(subscription, payload, options)
+            .then(function () {
+                res.sendStatus(201);
+            })
+            .catch(function (error) {
+                console.log(error);
+                res.sendStatus(500);
+            });
+        }, req.body.delay * 1000);
+});
+
+app.get('/save-vapid', async (req, res) => {
     
     const { name = 'World' } = req.query
     res.status(200).send(`Namaste ${name}!`)
