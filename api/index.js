b20426a35e2ebb1c4d958bfbe0b4a56390e9ac87
ayzom <59090251+ayzom@users.noreply.github.com>
2021-02-14 14:50:07 +0000

    push function update

--- api/index.js
+++ api/index.js
@@ -1,26 +1,21 @@
-const axios = require('axios');
-
 module.exports = async (req, res) => {
 
-    var data = JSON.stringify({"device":"samsung"});
-
-    var config = {
-    method: 'put',
-    url: 'https://ayzompush.firebaseio.com/index.json',
-    headers: { 
-        'Content-Type': 'application/json'
-    },
-    data : data
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    
+    var raw = JSON.stringify({"device":"samsung123"});
+    
+    var requestOptions = {
+      method: 'PUT',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
     };
-
-    await axios(config)
-    .then(function (response) {
-    console.log(JSON.stringify(response.data));
-
-    })
-    .catch(function (error) {
-    console.log(error);
-    });
+    
+    await fetch("https://ayzompush.firebaseio.com/index.json", requestOptions)
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
     
     const { name = 'World' } = req.query
     res.status(200).send(`Namaste ${name}!`)
